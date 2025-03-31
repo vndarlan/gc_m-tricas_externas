d02b362cc48c6a9bbf7bcea3b77887a4afde343f
Vinícius Darlan <viniciuschegouoperacional@gmail.com>
2025-03-31 09:44:44 -0300

    Update dropi_+_shopify.py

--- vendas/dropi_+_shopify.py
+++ vendas/dropi_+_shopify.py
@@ -2552,6 +2552,7 @@ def display_effectiveness_table(store_id, start_date_str, end_date_str):
         # Exibir tabela estilizada completa (não editável)
         st.dataframe(
             styled_view,
+            height=None,
             column_config={
                 "image_url": st.column_config.ImageColumn("Imagem", help="Imagem do produto"),
                 "product_display": "Produto",
